0ebc47a558df21281e815d162756e14ae3aca240
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-03-14 14:36:28 -0500

    Initial Commit

---
+++ MShop/MShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MShopApp.swift
+//  MShop
+//
+//  Created by Sravanthi Chinthireddy on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
